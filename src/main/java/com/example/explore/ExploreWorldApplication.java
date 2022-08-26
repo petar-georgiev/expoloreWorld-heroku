7b2756106aff6b3c612a03a84870487a7b0354ae
petar-georgiev <georgiev.peter1@gmail.com>
2022-08-26 19:21:46 +0300

    heroku properties

--- src/main/java/com/example/explore/ExploreWorldApplication.java
+++ src/main/java/com/example/explore/ExploreWorldApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExploreWorldApplication {
 
-    //Test
-
     public static void main(String[] args) {
         SpringApplication.run(ExploreWorldApplication.class, args);
     }
